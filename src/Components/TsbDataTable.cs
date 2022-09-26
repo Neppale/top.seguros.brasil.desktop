7dd512bed60ebaf2655270f29d507572cf3a6d5a
Vitor Costa <vitorkostadesign@gmail.com>
2022-09-25 23:47:25 -0300

    TsbTable with GET, POST, PUT and DELETE methods added. Now the table is dynamic.

--- src/Components/TsbDataTable.cs
+++ src/Components/TsbDataTable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,65 +10,126 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Top_Seguros_Brasil_Desktop.src.Models;
+using Top_Seguros_Brasil_Desktop.Utils;
+using Top_Seguros_Brasil_Desktop.src.Panels;
+using Newtonsoft.Json;
+using System.Collections;
 
 namespace Top_Seguros_Brasil_Desktop.src.Components
 {
     public partial class TsbDataTable : DataGridView
     {
+
+        private string address { get; set; }
         private string? selectedId { get; set; }
-        
+
+        public static ArrayList selectedRowValues = new ArrayList();
+
+        EngineInterpreter engineInterpreter = new EngineInterpreter(BasePanel.token);
+
         public TsbDataTable()
         {
-            SetupDataTable();
-            ActionColumnSetup();
-            OrganizeColumns();
-            
+           
+        }
+
+        public TsbDataTable(string adress, Type type)
+        {
+            this.address = adress;
         }
         
+        public async void Get<Type>()
+        {
+            DataTable dataTable = new DataTable();
+
+            int page = 1;
+            var response = await engineInterpreter.Request<IEnumerable<Type>>($"{this.address}", "GET", null);
+            IEnumerable<Type> responseBody = response.Body;
+
+
+            string[] properties = responseBody.First().GetType().GetProperties().Select(x => x.Name).ToArray();
+            try
+            {
+                foreach(var property in properties)
+                {
+                    dataTable.Columns.Add(property);
+                }
+
+                foreach (var item in responseBody)
+                {
+                    DataRow row = dataTable.NewRow();
+                    foreach (var property in properties)
+                    {
+                        row[property] = item.GetType().GetProperty(property).GetValue(item);
+                    }
+                    dataTable.Rows.Add(row);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            LoadData(dataTable);
+        }
+
+        public async void Post<Type>(object body)
+        {
+            
+            var json = JsonConvert.SerializeObject(body);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await engineInterpreter.Request<Type>($"{this.address}", "POST", data);
+
+            if (response.StatusCode == 201)
+            {
+                MessageBox.Show("Cadastrado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Erro ao cadastrar! " + response.Body.message);
+            }
+            
+            Get<Type>();
+            
+        }
+
+        public async void Put<Type>(object body)
+        {
+            var json = JsonConvert.SerializeObject(body);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await engineInterpreter.Request<Type>($"{this.address}", "PUT", data);
+
+            if(response.StatusCode == 201)
+            {
+                MessageBox.Show("Atualizado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Erro ao atualizar! " + response.Body.message);
+            }
+        }
+        
+        public async void Delete<Type>(string id)
+        {
+            var response = await engineInterpreter.Request<IEnumerable<Type>>($"{ this.address}+{id}", "DELETE", null);
+        }
+
         public TsbDataTable(IContainer container)
         {
             container.Add(this);
             InitializeComponent();
         }
 
-        //public void selectId(object sender, DataGridViewCellEventArgs e)
-        //{
-        //    var grid = (DataGridView)sender;
-
-        //    if (e.RowIndex < 0)
-        //    {
-        //        return;
-        //    }
-
-        //    if (e.ColumnIndex == this.Columns["Deletar"].Index && e.RowIndex >= 0)
-        //    {
-        //        selectedId = this.SelectedRows[0].Cells[2].Value.ToString();
-        //        MessageBox.Show(selectedId);
-        //    }      
-        //}
-
         public string getSelectedId()
         {
             return selectedId;
         }
 
-        public void removeRow(int id)
-        {
-            
-            foreach (DataGridViewRow row in this.Rows)
-            {
-                if (row.Cells[0].Value.ToString() == id.ToString())
-                {
-                    this.Rows.Remove(row);
-                    MessageBox.Show("Removido com sucesso!");
-                }
-            }
-
-        }
-
         public void LoadData(DataTable source)
         {
             this.DataSource = source;
+            ActionColumnSetup();
+            OrganizeColumns();
+            SetupDataTable();
             InitializeComponent();
         }
         
@@ -92,14 +154,16 @@ namespace Top_Seguros_Brasil_Desktop.src.Components
             delete.DefaultCellStyle.ForeColor = TsbColor.neutralGray;
             delete.DefaultCellStyle.Font = new Font(this.Font, FontStyle.Bold);
             delete.UseColumnTextForButtonValue = true;
-    
+
+            this.CellClick += new DataGridViewCellEventHandler(DeleteButton_Click);
+            this.CellClick += new DataGridViewCellEventHandler(EditButton_Click);
+
             this.Columns.Add(edit);
             this.Columns.Add(delete);
         }
 
         private void OrganizeColumns()
         {
-            this.Columns["Editar"].Visible = true;
 
             this.DataBindingComplete += (sender, e) =>
             {
@@ -131,7 +195,6 @@ namespace Top_Seguros_Brasil_Desktop.src.Components
             this.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.ReadOnly = true;
             this.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.RowTemplate.Height = 52;
             this.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             this.MultiSelect = false;
             this.BorderStyle = BorderStyle.Fixed3D;
@@ -140,7 +203,57 @@ namespace Top_Seguros_Brasil_Desktop.src.Components
             this.CellBorderStyle = DataGridViewCellBorderStyle.None;
             this.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
             this.AllowUserToAddRows = false;
-            this.BackgroundColor = TsbColor.surface;   
+            this.BackgroundColor = TsbColor.surface;
+            
+            
+
         }
+
+        private void DeleteButton_Click(object? sender, DataGridViewCellEventArgs e)
+        {
+            var grid = (DataGridView)sender;
+
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+
+            if (e.ColumnIndex == this.Columns["Deletar"].Index && e.RowIndex >= 0)
+            {
+                string selectedId = this.SelectedRows[0].Cells[0].Value.ToString();
+                if(MessageBox.Show(
+                    "Deseja realmente deletar o registro " + selectedId + "?",
+                    "Deletar registro",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2
+                    ) == DialogResult.Yes )
+                    { Delete<Type>(selectedId); };
+
+            }
+        }
+
+        private void EditButton_Click(object? sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+
+            if (e.ColumnIndex == Columns["Editar"].Index && e.RowIndex >= 0)
+            {
+
+                for (int i = 0; i < Columns.Count; i++)
+                {
+                    selectedRowValues.Add(SelectedRows[0].Cells[i].Value.ToString());
+                }
+
+                string selectedId = SelectedRows[0].Cells[0].Value.ToString();
+                MessageBox.Show(selectedId);
+            }
+
+        }
+
     }
 }
---
+++ src/Models/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_Seguros_Brasil_Desktop.src.Models
+{
+    public class Cliente
+    {
+        public int id_cliente { get; set; }
+        public string nome_completo { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+        public string cnh { get; set; }
+        public string cep { get; set; }
+        public string data_nascimento { get; set; }
+        public string telefone1 { get; set; }
+        public string telefone2 { get; set; }
+        public bool status { get; set; }
+        
+        public Cliente(string nomeCompleto, string email, string senha, string cpf, string cnh, string cep, string dataNascimento, string telefone1, string telefone2, bool status)
+        {
+            this.nome_completo = nomeCompleto;
+            this.email = email;
+            this.senha = senha;
+            this.cpf = cpf;
+            this.cnh = cnh;
+            this.cep = cep;
+            this.data_nascimento = dataNascimento;
+            this.telefone1 = telefone1;
+            this.telefone2 = telefone2;
+            this.status = false;
+        }
+    }
+
+
+    
+}
